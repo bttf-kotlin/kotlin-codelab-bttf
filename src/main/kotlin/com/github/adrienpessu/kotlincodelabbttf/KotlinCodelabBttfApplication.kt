e611ed6f796d886be85b2c90b88fe8bf075998b5
Adrien Pessu <adrien.pessu@keendo.fr>
2018-01-11 16:57:18 +0100

    chore(init) : add project from start.spring.io

---
+++ src/main/kotlin/com/github/adrienpessu/kotlincodelabbttf/KotlinCodelabBttfApplication.kt
@@ -0,0 +1,11 @@
+package com.github.adrienpessu.kotlincodelabbttf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCodelabBttfApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCodelabBttfApplication>(*args)
+}
